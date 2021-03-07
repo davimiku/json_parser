82dfb45084130df91711d6653fd3eafd1666d545
David Mikulis <davidmikulis@gmail.com>
2021-03-06 20:31:58 -0600

    Implement fmt::Display for values
    Potentially can use this to stringify values in the future

--- src/value.rs
+++ src/value.rs
@@ -1,9 +1,21 @@
-
-#[derive(Clone, PartialEq, Eq)]
+use std::fmt;
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Value {
     Number(i64),
     String(String),
     Boolean(bool),
     Array(Vec<Value>),
     Null,
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Number(n) => write!(f, "{}", n),
+            Value::String(s) => write!(f, "{}", s),
+            Value::Boolean(b) => write!(f, "{}", b),
+            Value::Array(v) => write!(f, "{:?}", v),
+            Value::Null => f.write_str("null"),
+        }
+    }
 }
