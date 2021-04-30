f8543f78ad98344203eeaad0227346300ac99245
David Mikulis <davidmikulis@gmail.com>
2021-04-29 19:09:13 -0500

    Export two functions for public use
    Provide parsing with the two parsers in this repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub mod lexer;
+use value::Value;
+
+mod combinator_parser;
+mod iterator_parser;
 mod location;
-pub mod parser;
-mod token;
+
 pub mod value;
+
+pub fn iterator_parse(input: &str) -> iterator_parser::ParseResult {
+    iterator_parser::parse(input)
+}
+
+pub fn combinator_parse(input: &str) -> Result<Value, &str> {
+    combinator_parser::parse(input)
+}
