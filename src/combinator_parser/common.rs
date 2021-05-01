d2f9b0d4cc582b934f4857c555fa55e88c8d033c
David Mikulis <davidmikulis@gmail.com>
2021-05-01 10:35:17 -0500

    Re-order parsers
    zero_or_more should go before one_or_more

--- src/combinator_parser/common.rs
+++ src/combinator_parser/common.rs
@@ -66,6 +66,23 @@ where
     map(pair(parser1, parser2), |(_left, right)| right)
 }
 
+/// Parses zero or more matches of the provided parser
+pub(crate) fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>,
+{
+    move |mut input| {
+        let mut result = Vec::new();
+
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item);
+        }
+
+        Ok((input, result))
+    }
+}
+
 /// Parses where there is at least one match for the provided parser
 pub(crate) fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
 where
@@ -80,23 +97,6 @@ where
         } else {
             return Err(input);
         }
-
-        while let Ok((next_input, next_item)) = parser.parse(input) {
-            input = next_input;
-            result.push(next_item);
-        }
-
-        Ok((input, result))
-    }
-}
-
-/// Parses zero or more matches of the provided parser
-pub(crate) fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
-where
-    P: Parser<'a, A>,
-{
-    move |mut input| {
-        let mut result = Vec::new();
 
         while let Ok((next_input, next_item)) = parser.parse(input) {
             input = next_input;
